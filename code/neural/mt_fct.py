fa8a15c8f6dae77ddfc56f1eed6ddfc76969d554
mathiasroesler <mathias.roesler@etu.upmc.fr>
2020-05-03 10:43:59 +0200

    Modified machine teaching function, removed test_data and test_labels from function because not needed.

--- code/neural/mt_fct.py
+++ code/neural/mt_fct.py
@@ -16,7 +16,7 @@ from init_fct import *
 from custom_fct import *
 
 
-def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coef, set_limit, batch_size=32, epochs=10):
+def create_teacher_set(train_data, train_labels, lam_coef, set_limit, batch_size=32, epochs=10):
     """ Produces the optimal teaching set given the train_data and
     a lambda coefficiant. If the teacher cannot converge in 
     ite_max_nb then it returns None.
@@ -28,14 +28,6 @@ def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coe
                 with the train data.
                 First dimension, number of examples.
                 Second dimension, one hot label.
-            test_data -> tf.tensor[float32], list of examples.
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            test_labels -> tf.tensor[int], list of labels associated
-                with the test data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
             lam_coef -> int, coefficiant for the exponential distribution
                 for the thresholds.
             set_limit -> int, maximum number of examples to be put in the 
@@ -45,7 +37,6 @@ def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coe
                 Second dimension features.
             teaching_labels -> np.array[int], list of labels associated with the
                 teaching data.
-            accuracy -> np.array[int], accuracy of the model at each iteration.
             teaching_set_len -> np.array[int], number of examples in teaching set at
                 each iteration.
     """
@@ -69,8 +60,8 @@ def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coe
     model, teaching_data, teaching_labels = teacher_initialization(model, train_data, train_labels, positive_index, negative_index, batch_size=batch_size, epochs=epochs)
 
     # Remove inital examples from the data and labels
-    train_data = np.delete(train_data, [positive_index, negative_index], axis=0)
-    train_labels = np.delete(train_labels, [positive_index, negative_index], axis=0)
+    # train_data = np.delete(train_data, [positive_index, negative_index], axis=0)
+    # train_labels = np.delete(train_labels, [positive_index, negative_index], axis=0)
 
     while len(teaching_data) != nb_examples and len(teaching_data) < set_limit:
         # Exit if all of the examples are in the teaching set or enough examples in the teaching set
@@ -86,27 +77,22 @@ def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coe
             # All examples are placed correctly
             break
 
-        added_indices = select_rndm_examples(missed_indices, 200)
-        #added_indices = select_examples(missed_indices, thresholds, weights)
+        #added_indices = select_rndm_examples(missed_indices, 200)
+        added_indices = select_examples(missed_indices, thresholds, weights)
         #added_indices = select_min_avg_dist(missed_indices, 200, train_data, train_labels, positive_average, negative_average)
         #added_indices = select_curriculum_examples(200, train_data, train_labels, ite-1)
 
         teaching_data, teaching_labels = update_teaching_set(teaching_data, teaching_labels, train_data, train_labels, added_indices)
         
-        curr_accuracy = update_model(model, teaching_data, teaching_labels, test_data, test_labels, batch_size=batch_size, epochs=epochs)
+        # Update model
+        model.fit(teaching_data, teaching_labels, batch_size=batch_size, epochs=epochs) 
 
         # Reset the weights for the cnn model
-        model = student_model(train_data[0].shape)
+        #model = student_model(train_data[0].shape)
 
-        # Test model accuracy
-        accuracy = np.concatenate((accuracy, [curr_accuracy]), axis=0)
+        # Add new length of teacher set
         teaching_set_len = np.concatenate((teaching_set_len, [len(teaching_data)]), axis=0)
     
-         
-        if abs((accuracy[-1] - accuracy[-2])) < 0.001: 
-            # If the performances don't change much
-            break
-         
 
         # Remove train data and labels, weights and thresholds of examples in the teaching set
         #train_data = np.delete(train_data, added_indices, axis=0)
@@ -117,12 +103,12 @@ def create_teacher_set(train_data, train_labels, test_data, test_labels, lam_coe
 
     print("\nIteration number:", ite)
 
-    return teaching_data, teaching_labels, accuracy, teaching_set_len, missed_set_len
+    return teaching_data, teaching_labels, teaching_set_len, missed_set_len
 
 
 def train_student_model(train_data, train_labels, test_data, test_labels, batch_size=32, epochs=10):
     """ Trains a student model fitted with the train set and
-    tested with the test set. Returns None if an error occured.
+    tested with the test set. 
     Input:  train_data -> tf.tensor[float32], list
                 of examples. 
                 First dimension, number of examples.
@@ -149,10 +135,6 @@ def train_student_model(train_data, train_labels, test_data, test_labels, batch_
 
     model = student_model(train_data[0].shape) # Declare student model
     
-    if model == None:
-        print("Error in function train_student_model: the model was not created.")
-        return None
-
     print("\nSet length", len(train_data))
 
     hist = model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs)
@@ -197,37 +179,3 @@ def update_teaching_set(teaching_data, teaching_labels, data, labels, added_indi
 
     return teaching_data, teaching_labels
 
-
-def update_model(model, teaching_data, teaching_labels, test_data, test_labels, batch_size=32, epochs=10):
-    """ Updates the student model using the teaching data and labels and evaluates the new 
-    performances with the test data and labels.
-    Input:  model -> cnn model, student model.
-            teaching_data -> tf.tensor[float32], list of examples. 
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            teaching_labels -> tf.tensor[int], list of labels associated
-                with the teaching data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
-            test_data -> tf.tensor[float32], list of examples.
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            test_labels -> tf.tensor[int], list of labels associated
-                with the test data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
-            batch_size -> int, number of examples used in a batch for the neural
-                network.
-            epochs -> int, number of epochs for the training of the neural network.
-    Output: accuracy -> float, score obtained with the updated model on the test data.
-    """
-
-    # Update model
-    model.fit(teaching_data, teaching_labels, batch_size=batch_size, epochs=epochs) 
-
-    # Test the updated model
-    accuracy = model.evaluate(test_data, test_labels, batch_size=batch_size)
-
-    return accuracy[1]
