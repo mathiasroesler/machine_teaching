04e1898758ff52766dea2e0f68565b93892b9e5b
mathiasroesler <mathias.roesler@etu.upmc.fr>
2020-05-06 12:35:45 +0200

    Optimized machine teaching functions.

--- code/neural/mt_fct.py
+++ code/neural/mt_fct.py
@@ -52,16 +52,11 @@ def create_teacher_set(train_data, train_labels, lam_coef, set_limit, batch_size
     thresholds = rng.exponential(1/lam_coef, size=(nb_examples)) # Threshold for each example
     accuracy = np.array([0], dtype=np.intc) # List of accuracy at each iteration, starts with 0
     teaching_set_len = np.array([0], dtype=np.intc) # List of number of examples at each iteration, starts with 0
-    missed_set_len = np.array([len(train_data)], dtype=np.intc) # List of number of misclassified examples at each iteration
     model = student_model(train_data[0].shape) # Declare student model
 
     positive_index, negative_index = rndm_init(train_labels) # Find index of intial examples
 
     model, teaching_data, teaching_labels = teacher_initialization(model, train_data, train_labels, positive_index, negative_index, batch_size=batch_size, epochs=epochs)
-
-    # Remove inital examples from the data and labels
-    # train_data = np.delete(train_data, [positive_index, negative_index], axis=0)
-    # train_labels = np.delete(train_labels, [positive_index, negative_index], axis=0)
 
     while len(teaching_data) != nb_examples and len(teaching_data) < set_limit:
         # Exit if all of the examples are in the teaching set or enough examples in the teaching set
@@ -71,7 +66,6 @@ def create_teacher_set(train_data, train_labels, lam_coef, set_limit, batch_size
 
         # Find all the missed examples indices
         missed_indices = np.unique(np.nonzero(np.round(model.predict(train_data)) != train_labels)[0])
-        missed_set_len = np.concatenate((missed_set_len, [len(missed_indices)]), axis=0)
     
         if missed_indices.size == 0:
             # All examples are placed correctly
@@ -82,7 +76,8 @@ def create_teacher_set(train_data, train_labels, lam_coef, set_limit, batch_size
         #added_indices = select_min_avg_dist(missed_indices, 200, train_data, train_labels, positive_average, negative_average)
 
         # Update the teacher set and add length to list
-        teaching_data, teaching_labels = update_teaching_set(teaching_data, teaching_labels, train_data, train_labels, added_indices)
+        teaching_data = tf.concat([teaching_data, tf.gather(train_data, added_indices)], axis=0)
+        teaching_labels = tf.concat([teaching_labels, tf.gather(train_labels, added_indices)], axis=0)
         teaching_set_len = np.concatenate((teaching_set_len, [len(teaching_data)]), axis=0)
     
         # Update model
@@ -95,21 +90,11 @@ def create_teacher_set(train_data, train_labels, lam_coef, set_limit, batch_size
 
         old_acc = new_acc
 
-        # Reset the weights for the cnn model
-        #model = student_model(train_data[0].shape)
-
-        """
-        # Remove train data and labels, weights and thresholds of examples in the teaching set
-        train_data = np.delete(train_data, added_indices, axis=0)
-        train_labels = np.delete(train_labels, added_indices, axis=0)
-        weights = np.delete(weights, added_indices, axis=0)
-        thresholds = np.delete(thresholds, added_indices, axis=0)
-        """
         ite += 1
 
     print("\nIteration number:", ite)
 
-    return teaching_data, teaching_labels, teaching_set_len, missed_set_len
+    return teaching_data, teaching_labels, teaching_set_len
 
 
 def train_student_model(train_data, train_labels, test_data, test_labels, batch_size=32, epochs=10):
@@ -147,41 +132,3 @@ def train_student_model(train_data, train_labels, test_data, test_labels, batch_
     test_score = model.evaluate(test_data, test_labels, batch_size=batch_size)
 
     return np.array(np.append(hist.history.get('accuracy'), [test_score[1]]), dtype=np.float32)
-
-
-def update_teaching_set(teaching_data, teaching_labels, data, labels, added_indices):
-    """ Updates the teaching data and labels using the train data and labels and the indices of the examples
-    to be added depending on the model type.
-    Input:  teaching_data -> tf.tensor[float32], list of examples. 
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            teaching_labels -> tf.tensor[int], list of labels associated
-                with the teaching data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
-            data -> tf.tensor[float32], list of examples.
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            labels -> tf.tensor[int], list of labels associated
-                with the data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
-            added_indices -> np.array[int], list of indices of examples to be added to the
-                teaching set.
-    Output: teaching_data -> tf.tensor[float32], list of examples. 
-                First dimension, number of examples.
-                Second and third dimensions, image. 
-                Fourth dimension, color channel. 
-            teaching_labels -> tf.tensor[int], list of labels associated
-                with the teaching data.
-                First dimension, number of examples.
-                Second dimension, one hot label.
-    """
-
-    teaching_data = tf.concat([teaching_data, tf.gather(data, added_indices)], axis=0)
-    teaching_labels = tf.concat([teaching_labels, tf.gather(labels, added_indices)], axis=0)
-
-    return teaching_data, teaching_labels
-
