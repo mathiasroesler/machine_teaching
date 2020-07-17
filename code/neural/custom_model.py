Fixed issue with the averaging of the accuracies and losses.

code/neural/custom_model.py
@@ -174,9 +174,8 @@ class CustomModel(object):
 
 
     def reset_model(self, input_shape, archi_type=1):
-        """ Resets attributes of the model.
+        """ Resets the weights of the model.
 
-        The model is recreated and the accuracies and losses are reset.
         The architecture depends on the variable archi_type.
         1 for LeNet5, 2 for All-CNN, 3 for CNN.
         Input:  input_shape -> tuple[int], shape of the input data. 
@@ -188,12 +187,6 @@ class CustomModel(object):
         # Reset weigths
         self.model = self.set_model(input_shape, archi_type)
 
-        # Reset accuracies and losses
-        self.train_acc = np.array([], dtype=np.float32)
-        self.val_acc = np.array([], dtype=np.float32)
-        self.train_loss = np.array([], dtype=np.float32)
-        self.val_loss = np.array([], dtype=np.float32)
-    
 
     def train(self, train_data, train_labels, val_set, strategy, epochs=10,
             batch_size=32, verbose=0):
@@ -242,6 +235,12 @@ class CustomModel(object):
             # If the labels are not one hot
             self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy(
                     from_logits=True)
+
+        # Initialize arrays
+        self.train_acc = np.zeros(epochs, dtype=np.float32)
+        self.train_loss = np.zeros(epochs, dtype=np.float32)
+        self.val_acc = np.zeros(epochs, dtype=np.float32)
+        self.val_loss = np.zeros(epochs, dtype=np.float32)
 
         if strategy == "MT" or strategy == "Full":
             self.simple_train(train_data, train_labels, val_set, epochs,
@@ -356,24 +355,29 @@ class CustomModel(object):
                 monitor='val_loss', patience=1)
 
         curriculum_indices = two_step_curriculum(train_data, train_labels)
+        curriculum_len = len(curriculum_indices)
+        half_epoch = epochs//curriculum_len
 
         # Train model with easy then hard examples
-        for i in range(len(curriculum_indices)):
+        for i in range(curriculum_len):
             hist = self.model.fit(
                     tf.gather(train_data, curriculum_indices[i]),
                     tf.gather(train_labels, curriculum_indices[i]),
                     validation_data=val_set, callbacks=[stop_callback],
-                    batch_size=batch_size, epochs=epochs//2, verbose=verbose)
+                    batch_size=batch_size, epochs=half_epoch, verbose=verbose)
+
+            real_epoch = len(hist.history.get('accuracy'))
+            origin = i * half_epoch
 
             # Save accuracies and losses
-            self.train_acc = np.concatenate((self.train_acc, 
-                hist.history.get('accuracy')), axis=0) 
-            self.train_loss = np.concatenate((self.train_loss, 
-                hist.history.get('loss')), axis=0)
-            self.val_acc = np.concatenate((self.val_acc, 
-                hist.history.get('val_accuracy')), axis=0)
-            self.val_loss = np.concatenate((self.val_loss, 
-                hist.history.get('val_loss')), axis=0)
+            self.train_acc[origin:real_epoch+origin] =  hist.history.get(
+                    'accuracy')
+            self.train_loss[origin:real_epoch+origin] =  hist.history.get(
+                    'loss')
+            self.val_acc[origin:real_epoch+origin] =  hist.history.get(
+                    'val_accuracy')
+            self.val_loss[origin:real_epoch+origin] =  hist.history.get(
+                    'val_loss')
 
 
     def SPL_train(self, train_data, train_labels, val_set, epochs=10,

code/neural/main.py
@@ -53,14 +53,14 @@ if __name__ == "__main__":
 
     # Variables for neural networks
     archi_type = 1
-    epochs = 2
+    epochs = 10
     batch_size = 128
 
     # Variables for plotting
     plot_types = ['ro-', 'bo-', 'go-', 'ko-']
 
     # Other variables
-    strat_names = ["Full", "MT", "CL", "SPL"]
+    strat_names = ["CL"]#["Full", "MT", "CL", "SPL"]
     iteration_nb = 3 
     class_nb = -1  # Class number for one vs all
     sparse = False  # If labels are to be sparse or not
@@ -117,12 +117,12 @@ if __name__ == "__main__":
     for strat in strat_names:
         # Initialize dictionaries
         time_dict[strat] = 0
-        train_acc_dict[strat] = [0]
+        train_acc_dict[strat] = np.zeros(epochs)
         model_dict[strat] = CustomModel(data_shape, max_class_nb, archi_type,
                 warm_up, threshold, growth_rate)
-        train_loss_dict[strat] = [0]
-        val_loss_dict[strat] = [0]
-        normalizer_dict[strat] = [0]
+        train_loss_dict[strat] = np.zeros(epochs)
+        val_loss_dict[strat] = np.zeros(epochs)
+        normalizer_dict[strat] = np.zeros(epochs)
 
     for i in range(iteration_nb):
         print("\nITERATION", i+1)
@@ -148,15 +148,15 @@ if __name__ == "__main__":
             # Test model
             model.test(test_data, test_labels)
 
-            # Save accuracy, losses and time
+            # Save accuracy, losses and 
             train_acc_dict.update({strat: update_dict(
                 train_acc_dict.get(strat), model.train_acc)})
             train_loss_dict.update({strat: update_dict(
                 train_loss_dict.get(strat), model.train_loss)})
             val_loss_dict.update({strat: update_dict(
                 val_loss_dict.get(strat), model.val_loss)})
-            normalizer_dict.update({strat: update_dict(
-                normalizer_dict.get(strat), np.ones(len(model.train_acc)))})
+            normalizer_dict.get(strat)[np.argwhere(train_acc_dict.get(
+                strat))] += 1
             time_dict.update({strat: time_dict.get(strat) + toc-tic})
 
             # Reset model
@@ -164,14 +164,20 @@ if __name__ == "__main__":
 
     # Average time, accuracies and losses
     for strat in strat_names:
-        train_acc_dict.update({strat: np.round(
-            train_acc_dict.get(strat)/iteration_nb, decimals=2)})
         time_dict.update({strat: np.round(
             time_dict.get(strat)/iteration_nb, decimals=2)})
+        train_acc_dict.update({strat: np.round(
+            train_acc_dict.get(strat)[train_acc_dict.get(strat) != 0] / 
+            normalizer_dict.get(strat)[normalizer_dict.get(strat) != 0],
+            decimals=2)})
         train_loss_dict.update({strat: np.round(
-            train_loss_dict.get(strat)/iteration_nb, decimals=2)})
+            train_loss_dict.get(strat)[train_loss_dict.get(strat) != 0] /
+            normalizer_dict.get(strat)[normalizer_dict.get(strat) != 0],
+            decimals=2)})
         val_loss_dict.update({strat: np.round(
-            val_loss_dict.get(strat)/iteration_nb, decimals=2)})
+            val_loss_dict.get(strat)[val_loss_dict.get(strat) != 0] /
+            normalizer_dict.get(strat)[normalizer_dict.get(strat) != 0],
+            decimals=2)})
         test_acc_dict[strat] = model_dict.get(strat).test_acc
 
     # Save results in file